534f0cdb21e061a348212e9b53d1dc895d6a692f
Sergey Patrakov <sergeypatrakov@MacBook-Pro-Sergey.local>
2022-08-14 15:12:03 +0300

    Проект готов

---
+++ yatube/posts/views.py
@@ -0,0 +1,25 @@
+from gc import get_objects
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+
+
+def index(request):
+    title = 'Посление обновления на сайте'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    title = 'Записи сообщества'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, template, context)
